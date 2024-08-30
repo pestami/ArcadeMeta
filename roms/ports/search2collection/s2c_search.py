c74d0ad6af19aa1e0032f74fab52c395ce4e340e
PatMat <51946906+pestami@users.noreply.github.com>
2024-08-30 09:56:54 +0100

    Update Script created
    Wget implemented

--- roms/ports/search2collection/s2c_search.py
+++ roms/ports/search2collection/s2c_search.py
@@ -10,7 +10,9 @@
 import sqlite3
 from colors import colors
 import os
-
+import re
+from xml.dom.minidom import parse
+import xml.dom.minidom
 
 class s2c:
 
@@ -94,7 +96,7 @@ class s2c:
         
         #print(chr(27) + "[2J")
         os.system('cls||clear')
-        print('.........................................................')
+        print('---------------------------------------------------------')
         print('-----KEWORDS FOUND LIST------------------')
         sFound=str(len(result_List))
         
@@ -106,11 +108,13 @@ class s2c:
             print(items)
             if i==25:
                 i=0
-                print('Displayed: ' +str(nTotal) +'/'+ sFound +'  Press key to continue..........')
+                print('Displayed: ' +str(nTotal) +'/'+ sFound +'  Press key to continue, x to exit.........')
                 sCMD = str(input())   
+                if sCMD =='x' or sCMD =='X':
+                    break
             
         print('-----KEWORDS FOUND END-------------------')      
-        print('.........................................................')
+        print('---------------------------------------------------------')
     
 #-------------------------------------------------------------------------------
 #============================================================================
@@ -182,6 +186,25 @@ class s2c:
 #-------------------------------------------------------------------------------
 #============================================================================
 #===========================================================================
+    def DisplayGameLists():
+               
+                pathcollections='/home/pi/.emulationstation/gamelists/'             
+
+                dir_list = os.listdir(pathcollections)
+                i=0
+               
+                # prints all files
+                os.system('cls||clear')
+                print('.........................................................')
+                print('-----GAME Lists FOUND------------------')
+                print("Directory= '", pathcollections, "' :")
+                for sfiles in dir_list:                   
+                    print(str(i) + ": " + sfiles)
+                    i=i+1
+                return dir_list
+#-------------------------------------------------------------------------------
+#============================================================================
+#===========================================================================
     def DeleteCollection(collections_List):
             print('.........................................................')
             print('Type collection index Number, it will be deleted: ')
@@ -199,31 +222,222 @@ class s2c:
                     print("The system cannot find the file specified.")
                     print(file_path)
                     
-        
-        
-        
-#============================================================================
-#============================================================================
-    def Help(ID):
+    def DeleteSystem(sPathFileDB,sSystemName):
+            print('.........................................................')
+            print('Type systems name(NES, SNES etc.), it will be deleted: ')     
+            sSystemName = str(input())
+            sSystemName=sSystemName.replace(' ','')
             
+            con = sqlite3.connect(sPathFileDB)
+            con.text_factory = str
+            cur = con.cursor()
+            sSQL="DELETE FROM LIST_SEARCH_DB WHERE CONSOLE='" + sSystemName + "';"
+            print(sSQL)
+            cur.execute(sSQL)
+
+            print('Rows Deleted =' + str(cur.rowcount))
+##            sSQL_01="UPDATE LIST_GAMES_META SET CONSOLE = '"+console+"'; "
+##            cur.execute(sSQL_01)
+
+            con.commit()
+            con.close()
+        
+#============================================================================
+#============================================================================
+# ID='0123456789'0-clscr; 1-heading; 2-systems; 3-collections; 4-search keywrd; 5-save search; 6-admin
+# Help(ID)     
+    def Help(ID): 
+            
+            if ID=='':
+                ID='0123456789'
             #print(chr(27) + "[2J")
-            os.system('cls||clear')
-            print( colors.fg.green, "...")
-            print('=====================================================')
-            print('==Program to generate a playlist=======V20250527=====')
-            print('=====================================================')
-            print('type s keyword1+keyword2 to search for game in Name of ROM')
-            print('type sd keyword1+keyword2 to search for games in description')
-            print('type l  list last search results')
-            print('type wr to write playlist to a collection list and rename')
-            print('type w  to write playlist to collection list')
-            print('....................................................')
-            print('type lc to list collections')
-            print('type dc to delete a collection')
-            print('....................................................')
-            print('type h  to help')
-            print('type x  to quit')
-            print('=====================================================')
+            
+            if '0' in ID:
+                os.system('cls||clear')
+            if '1' in ID:
+                print( colors.fg.green, "...")
+                print('==========================================================')
+                print('==Program to generate a playlist=======V20250613==========')
+                print('==========================================================')
+           
+            if '2' in ID:
+                print( colors.fg.red, "...")
+                print('\n..Update Search DataBase................................')
+                print('type ls to list installed  game Systems')
+                print('type ws to write index game Systems to DataBase')
+                print('type ds to delete a Systems from the search DataBase')
+           
+            if '3' in ID:
+                print( colors.fg.purple, "...")
+                print('\n.............................................')
+                print('type lc to list collections')
+                print('type dc to delete a collection')
+           
+            if '4' in ID:
+                print( colors.fg.lightblue, "...")
+                print('\n..SEARCH for ROMS......................................')
+                print('type s keyword1+keyword2 to search for game in Name of ROM')
+                print('type sd keyword1+keyword2 to search for games in description')
+            
+            if '5' in ID:
+                print( colors.fg.lightblue, "...")
+                print('\n..SEARCH RESULTS.......................................')
+                print('type l  list last plalists search results')
+                print('type wr to write playlist to a collection list and rename')
+                print('type w  to write playlist to collection list')
+           
+            if '6' in ID:
+                print( colors.fg.green, "...")
+                print('\n........................................................')
+                print('type h  to help')
+                print('type x  to quit')
+                print('==========================================================')
 #-------------------------------------------------------------------------------
 #============================================================================
 #============================================================================
+    def ROM_DB_LoadGameListsContents(sPathFileDB,lLIST_XML_GAMESLISTS_Metadata,console):
+
+            # LIST_GAMES_META
+            #(PATH,NAME,DES,IMAGE,RATING,RELEASE,DEVELOPER,PUBLISHER,GENERE,PLAYERS,PLAYCOUNT,LASTPLAYED)
+            #/home/pi/RetroPie/roms/c64/Ping Pong.tap
+            lLIST_SEARCH_DB=[]
+            
+            #print(lLIST_XML_GAMESLISTS_Metadata[1])
+            
+            for items in  lLIST_XML_GAMESLISTS_Metadata:
+                NAMEROM= items[1]
+                CONSOLE= console  # not in gameslist.xml
+                SEARCHTEXT= items[1] +': '+ items[2]
+                COLLECTIONPATHFILE = items[0].replace('./','/home/pi/RetroPie/roms/'+CONSOLE +'/')
+                
+                lLIST_SEARCH_DB.append([NAMEROM,CONSOLE,SEARCHTEXT,COLLECTIONPATHFILE])
+            
+            con = sqlite3.connect(sPathFileDB)
+            con.text_factory = str
+            cur = con.cursor()
+            cur.executemany("INSERT OR IGNORE INTO LIST_SEARCH_DB (NAMEROM,CONSOLE,SEARCHTEXT,COLLECTIONPATHFILE) VALUES (?, ?, ?, ? );", lLIST_SEARCH_DB)
+
+            print('Rows uploaded into SEARCH DataBase =' + str(cur.rowcount))
+##            sSQL_01="UPDATE LIST_GAMES_META SET CONSOLE = '"+console+"'; "
+##            cur.execute(sSQL_01)
+
+            con.commit()
+            con.close()
+#============================================================================
+#============================================================================
+
+#============================================================================
+#============================================================================
+    def ExtractMetadatefromGameXML(xmlfile,sPathROMS, **kwargs):
+        
+        def GetElement(XMLElement,sTagName):
+                try:
+                    Name = XMLElement.getElementsByTagName(sTagName)[0]
+                    sData=Name.childNodes[0].data
+            ##        print(sTagName +" = " + sData)
+                    return sData
+                except:
+            ##        print(sTagName + " = Does Not Exist")
+                    return ''
+
+    ##     xmlfile = 'G:\\roms\\mame\\gamelist.xml'
+    ##    xmlfile = 'G:\\roms\\amstradcpc\\gamelist.xml'
+
+        flagDebug=kwargs.get('flagDebug', None)
+        print ('.......................................................')
+        print ('...  Extract Metadata from Game XML ...')
+        print (xmlfile)
+        #print ('...  lGamesListCollection...')
+        print ('.......................................................')
+
+        lGamesListCollection=[]
+        lGamesList=[]
+        lgames = os.listdir(sPathROMS)
+
+        # Open XML document using minidom parser
+        DOMTree = xml.dom.minidom.parse(xmlfile)
+        gameList = DOMTree.documentElement
+        # Gamelist has no attributes typicaly
+        #print ('LINE 271')
+        if gameList.hasAttribute("system"):
+           print ("Root element : %s" % gameList.getAttribute("system"))
+
+        # Get all the movies in the collection
+        GAMES = gameList.getElementsByTagName("game")
+
+        for game in GAMES:
+            lGamesList=[]
+            #(PATH,NAME,DES,IMAGE,RATING,RELEASE,DEVELOPER,PUBLISHER,GENERE,PLAYERS,PLAYCOUNT,LASTPLAYED)
+            if flagDebug: print ('...<GAME>' , xmlfile)
+            sValue = GetElement(game,'path')
+            sGameRom_File= re.split('/', sValue)[-1]   #last value in list]
+            lGamesList.append(sValue)
+            if flagDebug :print("path:"+sValue)
+
+            sValue = GetElement(game,'name')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("name:"+sValue)
+
+            sValue = GetElement(game,'desc')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("desc:"+sValue[:40])
+
+            sValue = GetElement(game,'image')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("image:"+sValue)
+
+            sValue = GetElement(game,'rating')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("rating:"+sValue)
+
+            sValue = GetElement(game,'releasedate')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("releasedate:"+sValue)
+
+            sValue = GetElement(game,'developer')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("developer:"+sValue)
+
+            sValue = GetElement(game,'publisher')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("publisher:"+sValue)
+
+            sValue = GetElement(game,'genre')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("genre:"+sValue)
+
+            sValue = GetElement(game,'players')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("players:"+sValue)
+
+            sValue = GetElement(game,'playcount')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("playcount:"+sValue)
+
+            sValue = GetElement(game,'lastplayed')
+            lGamesList.append(sValue)
+            if sValue and flagDebug: print("lastplayed:"+sValue)
+            if flagDebug: print ('...</GAME>')
+                
+            
+            if sGameRom_File in lgames:
+                lGamesListCollection.append(lGamesList)
+                
+    ##        print (lGamesList)
+        return lGamesListCollection, lgames
+        print ("Number of ROMS Detected in folder=" + lgames.count)
+        print ("Number of ROMS Listed in gamelist=" + lGamesListCollection.count)
+        print ("=============================================\n")
+        
+
+
+
+#============================================================================
+#============================================================================
+        
+###############################################################################
+if __name__ == '__main__':
+    
+    print ("======================================================")
+    print ("TEST CODE s2c_search.py===============================")
+    print ("======================================================")
